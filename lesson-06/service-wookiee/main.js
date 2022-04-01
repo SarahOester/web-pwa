78c79e499360b16b2b09ef88b3f3a22a90b215b6
Sarah Øster <eaasody@students.eaaa.dk>
2022-04-01 12:31:03 +0200

    changes

--- lesson-06/service-wookiee/main.js
+++ lesson-06/service-wookiee/main.js
@@ -1,6 +1,23 @@
 const template = document.querySelector("#template").content;
 const boxes = document.querySelector("#boxes");
 
+const count = document.querySelector("#request-count");
+let counts = 0;
+
+if("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("sw.js");
+}
+
+navigator.serviceWorker.addEventListener("message", (e) => {
+  console.log(e.data);
+  counter();
+});
+
+function counter(){
+  counts++;
+  count.innerHTML = counts;
+}
+
 for (const fileType of ["text/css", "text/html", "application/json"]) {
   // Clone the template for each box
   const box = template.cloneNode(true);
---
+++ lesson-06/service-wookiee/sw.js
@@ -0,0 +1,76 @@
+const cacheName = "v1";
+
+const cacheAssets = ["index.html", "main.js", "style.css", "chewie.jpg"];
+
+self.addEventListener("install", async (e) => {
+    console.log("installed");
+
+    e.waitUntil(
+        caches
+            .open(cacheName)
+            .then((cache) => {
+                console.log("ServiceWorker: Caching files");
+                cache.addAll(cacheAssets);
+            })
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener("activate", async (e) => {
+    console.log("activated");
+    //Remove unwanted caches
+    e.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== cacheName) {
+                        console.log("ServiceWorker: Clearing old caches");
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+
+self.addEventListener("fetch", (event) => {
+    console.log("ServiceWorker: Fetching 22");
+    const url = new URL(event.request.url);
+    let arr = url.pathname.split("/");
+
+    if (arr[arr.length - 1] == "fake.css") {
+        let res = new Response("/*CSS*/", {
+            headers: {
+                "content-type": "application/css",
+            },
+        });
+        event.respondWith(res);
+    } else if (arr[arr.length - 1] == "fake.html") {
+        let res = new Response("/*HTML*/", {
+            headers: {
+                "content-type": "application/html",
+            },
+        });
+        event.respondWith(res);
+    } else if (arr[arr.length - 1] == "fake.json") {
+        let res = new Response("/*JSON*/", {
+            headers: {
+                "content-type": "application/json",
+            },
+        });
+        event.respondWith(res);
+    }
+});  
+
+
+self.addEventListener("fetch", async (e) => {
+    console.log("fetched");
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+
+    let client = await clients.get(e.clientId);
+
+    if (client) {
+        client.postMessage("Besked fra SW")
+    }
+});
